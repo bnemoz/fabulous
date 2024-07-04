8cf8f063261fb33cc6e5a51666939bf8194a1a47
Benjamin Némoz <bnemoz@hotmail.com>
2024-07-04 19:17:55 +0200

    cleaning up code

--- fabulous/src/App.tsx
+++ fabulous/src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
 import fabulousLogo from '../public/fabulous.png'
-import * as React from "react";
-import {Image, StyleSheet, View, Text} from "react-native";
-import { Padding, Color, FontSize, FontFamily, Border, StyleVariable } from "../GlobalStyles";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
