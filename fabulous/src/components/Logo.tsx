435d3b931e798b94c92714af61bef93d2468ae54
Benjamin Némoz <bnemoz@hotmail.com>
2024-08-07 11:25:29 +0200

    Updated URL

--- fabulous/src/components/Logo.tsx
+++ fabulous/src/components/Logo.tsx
@@ -4,7 +4,7 @@ import fabulousLogo from "/fabulous.png";
 export default function Logo() {
   return (
     <Grid>
-      <a href="https://fabulous.nemoz.me">
+      <a href="https://fabulous-abs.com">
         <img src={fabulousLogo} className="logo fabulous" alt="Fabulous logo" />
       </a>
     </Grid>
